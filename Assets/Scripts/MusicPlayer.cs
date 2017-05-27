b7e2e033c94cf712222e2e5eb6c858b74b8a5f4a
Joel Francisco <joel.francisco@zenn.pt>
2017-05-27 05:17:11 +0100

    Add static scenes and game resources

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour
+{
+	static MusicPlayer instance = null;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		if (instance == null) {
+			instance = this;	
+			GameObject.DontDestroyOnLoad (gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
